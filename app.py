e1eedd1637260478d3ea6cc3782f43cbe3963232
YasuChiba <yasu.bv0309@gmail.com>
2019-04-18 22:00:53 +0900

    aa

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import RPi.GPIO as GPIO
 import dht11
 
 GPIO.cleanup()
+COUNT = 100
+DHT11_PIN = 7
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+dht11Instance = dht11.DHT11(pin=DHT11_PIN)
 
 def printTemperature():
-  result = instance.read()
+  result = dht11Instance.read()
   if result.is_valid():
     print("temperature: " + str(result.temperature))
   else:
