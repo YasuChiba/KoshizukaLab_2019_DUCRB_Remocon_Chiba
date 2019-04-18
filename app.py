de537833b2fb4a6a15b7d04433eddceb094713e1
YasuChiba <yasu.bv0309@gmail.com>
2019-04-18 21:24:07 +0900

    led

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import time
-
 import RPi.GPIO as GPIO
-import dht11
-import datetime
 
-# initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+
+COUNT = 100
+PIN = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN,GPIO.OUT)
+
+for _ in xrange(COUNT):
+    GPIO.output(PIN,True)
+    time.sleep(1.0)
+    GPIO.output(PIN,False)
+    time.sleep(1.0)
+
 GPIO.cleanup()
-# read data using pin 14
-instance = dht11.DHT11(pin=7)
-
-
-
-def getTemprature():
-		result = instance.read()
-		return result.temperature
-
-while True:
-		print(getTemprature())
-		time.sleep(5)
