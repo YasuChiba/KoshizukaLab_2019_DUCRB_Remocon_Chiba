84edd32b1bbc34f1e0bfce9200bd7099d445273b
YasuChiba <yasu.bv0309@gmail.com>
2019-04-18 23:26:02 +0900

    aa

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ import time
 import RPi.GPIO as GPIO
 import dht11
 
-GPIO.cleanup()
-COUNT = 100
-DHT11_PIN = 7
-GPIO.cleanup()
-GPIO.setmode(GPIO.BOARD)
-dht11Instance = dht11.DHT11(pin=DHT11_PIN)
+
+def setup():
+	GPIO.cleanup()
+	'''
+	COUNT = 100
+	DHT11_PIN = 7
+	GPIO.cleanup()
+	GPIO.setmode(GPIO.BOARD)
+	dht11Instance = dht11.DHT11(pin=DHT11_PIN)
+	'''
 
 def printTemperature():
   result = dht11Instance.read()
@@ -17,7 +21,7 @@ def printTemperature():
     print("cannot read temperature")
 
 
-
+setup()
 while True:
-	printTemperature()
+	#printTemperature()
 	time.sleep(50)
