b3b297c11775a239351022f6aaf1e277e59af4bf
YasuChiba <yasu.bv0309@gmail.com>
2019-04-19 18:14:35 +0900

    aa

--- infratedReceiverDecoder.c
+++ infratedReceiverDecoder.c
@@ -38,7 +38,7 @@ void useDigitalRead()
       currentMills = millis();
 
       resultArray[counter][0] = value;
-      resultArray[counter][1] = currentMills;
+      resultArray[counter][1] = currecurrentMills - previousMillsntMills;
 
       //printf("%d     %d\n", value, currentMills - previousMills);
       counter++;
