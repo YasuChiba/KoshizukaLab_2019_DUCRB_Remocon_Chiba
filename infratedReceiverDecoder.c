ad61edb7f53a4bac71869954188886a79c1ffd28
YasuChiba <yasu.bv0309@gmail.com>
2019-04-19 16:14:34 +0900

    aa

--- infratedReceiverDecoder.c
+++ infratedReceiverDecoder.c
@@ -4,16 +4,21 @@
 
 #define INFRATED_RECEIVER_PIN 0
 
+void fall()
+{
+  print("fall \n");
+}
+
 int main(void)
 {
   wiringPiSetup();
   pinMode(INFRATED_RECEIVER_PIN, INPUT);
   pullUpDnControl(INFRATED_RECEIVER_PIN, PUD_UP);
+  wiringPiISR(INFRATED_RECEIVER_PIN, INT_EDGE_FALLING, *fall);
   int result = 0;
-
   while (1)
   {
-    result = digitalRead(INFRATED_RECEIVER_PIN);
+    //result = digitalRead(INFRATED_RECEIVER_PIN);
     printf("%d       %d\n", result, millis());
     delay(100);
   }
