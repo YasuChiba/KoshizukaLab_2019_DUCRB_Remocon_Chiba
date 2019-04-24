bd815d62d439eaedad1af0840fbef7ca15449a4e
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-2.local>
2019-04-24 18:43:16 +0900

    aa

--- arduino/arduino-IR-transmitter/arduino-IR-transmitter.ino
+++ arduino/arduino-IR-transmitter/arduino-IR-transmitter.ino
@@ -1,6 +1,22 @@
 #include <avr/io.h>
 
+
 #define PWMPin 10
+
+void setup() {
+  pinMode(PWMPin, OUTPUT);
+  TCCR1A=0b00100010
+  TCCR1B=0b00011001
+  ICR1=420
+  OCR1A=210
+  
+}
+
+void loop() {
+  
+}
+
+/*
 
 unsigned int frq = 38000L; // 周波数
 float duty = 0.5; // 指定したいデューティ比
@@ -18,14 +34,30 @@ void setup() {
   OCR1B = (unsigned int)(1000000 / frq * duty);
 }
 
+void one() {
+  TCCR1B = 0b00010010;
+  delay(20);
+  TCCR1B = 0b00000000;
+  delay(20);
+}
+
+void zero() {
+  TCCR1B = 0b00010010;
+  delay(20);
+  TCCR1B = 0b00000000;
+  delay(10);
+}
+
 void loop() {
 
   while(1) {
-    pinMode(PWMPin, OUTPUT);
-    delay(20);
-    pinMode(PWMPin, INPUT);
-    delay(20);
-  }
+    one();
+    zero();
+    one();
+    one();
+    zero();
 
- 
+    
+  }
 }
+*/
