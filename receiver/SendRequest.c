e684e48c7448f11130376b720c747f2c2411491a
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-7.local>
2019-05-03 22:19:43 +0900

    aa

--- receiver/SendRequest.c
+++ receiver/SendRequest.c
@@ -7,18 +7,6 @@
 void turnOnLight(int roomNumber) {
     printf("turn on %d\n",roomNumber);
 
-    FILE* fp;
-    char  buf[1024];
-    //char* cmd = "python sendRequestPython.py 10";
-    char* cmd = "/usr/bin/ls -l /";
-
-   // printf("result : %d\n",system("python sendRequestPython.py 10"));
-    if ((fp = popen(cmd, "r")) != NULL) {
-        while (fgets(buf, sizeof(buf), fp) != NULL) {
-            printf("%s", buf);
-        }
-        pclose(fp);
-        return 0;
-    }
+    system("ls");
 
 }
