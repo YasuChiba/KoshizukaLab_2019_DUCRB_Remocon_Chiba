e2ad894f29d7ae8d7b8a74575c86c8ebf999f715
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-7.local>
2019-05-03 22:29:20 +0900

    aa

--- receiver/SendRequest.c
+++ receiver/SendRequest.c
@@ -1,7 +1,18 @@
 #include "sendRequest.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int test()
+{
+  char command[256];
+
+   if (snprintf(command, sizeof(command), "ls -l %s", "test.c") > sizeof(command)-1){
+    fprintf(stderr,"Length of the strings(filename) is too large.\n");
+    exit(1);
+  }
+  system(command);
+}
 
 
 void turnOnLight(int roomNumber) {
@@ -10,3 +21,4 @@ void turnOnLight(int roomNumber) {
     system("ls -l test.c");
 
 }
+
