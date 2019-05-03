eac4973e5c9ec6112e3439b9934bb89a15b785ed
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-7.local>
2019-05-03 21:43:52 +0900

    aa

--- receiver/sendRequestPython.py
+++ receiver/sendRequestPython.py
@@ -1,6 +1,22 @@
 import sys
+import requests
+import json
+import time
+import RPi.GPIO as GPIO
 
 
+def destroy():
+	GPIO.output(13, GPIO.LOW)     # led off
+	GPIO.cleanup()   
+
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+
+GPIO.output(infraredSensorPin, GPIO.HIGH)  # led on
+time.sleep(2)
+GPIO.output(infraredSensorPin, GPIO.LOW) # led off
 
 
 print("python called")
