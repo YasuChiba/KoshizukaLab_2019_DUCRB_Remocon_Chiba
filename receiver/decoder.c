314df2d2d4021bab7b408dad5880804e0f30193a
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-7.local>
2019-05-03 19:35:04 +0900

    aa

--- receiver/decoder.c
+++ receiver/decoder.c
@@ -5,9 +5,11 @@
 
 void decode(int* array) {
     //printf("turn on %d\n",roomNumber);
+    printf("start decoding \n");
     for(int i = 0; i < CODE_LENGTH; i++) {
         printf("%d ",array[i]);
     }
 
+    printf("end decoding \n");
 
 }
