13f6833fa8e34fe55ad8a3b315932e3a092be740
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-7.local>
2019-05-03 21:19:29 +0900

    aa

--- receiver/SendRequest.c
+++ receiver/SendRequest.c
@@ -6,5 +6,5 @@
 
 void turnOnLight(int roomNumber) {
     printf("turn on %d\n",roomNumber);
-    system("python sendRequestPython.py 100");
+    printf("result : %d\n",system("python sendRequestPython.py 100"));
 }
