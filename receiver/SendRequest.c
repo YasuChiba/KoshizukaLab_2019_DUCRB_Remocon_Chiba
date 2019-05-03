d749a581d05e02bd790d8612bd2e254cc0358f99
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-7.local>
2019-05-03 21:17:00 +0900

    aa

--- receiver/SendRequest.c
+++ receiver/SendRequest.c
@@ -1,8 +1,10 @@
 #include "sendRequest.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
 void turnOnLight(int roomNumber) {
     printf("turn on %d\n",roomNumber);
+    system("python sendRequestPython.py 100")
 }
---
+++ receiver/sendRequestPython.py
@@ -0,0 +1,10 @@
+import sys
+
+
+
+
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    print("rrrrrr  ", args[0])
