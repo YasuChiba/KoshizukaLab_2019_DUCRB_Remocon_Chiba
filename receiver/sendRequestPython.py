e57f22fc1a2909fe652a3fec1710880ebf663c02
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-169.local>
2019-05-07 16:27:17 +0900

    aa

--- receiver/sendRequestPython.py
+++ receiver/sendRequestPython.py
@@ -7,13 +7,13 @@ import RPi.GPIO as GPIO
 
 #isOn: bool, roomNumber: str
 def lightControll(isOn, roomNumber):
-    url = “http://172.26.16.8/api/ducrbcontrol/light/”
-    headers = {“Content-Type”: “application/json”}
+    url = "http://172.26.16.8/api/ducrbcontrol/light/"
+    headers = {"Content-Type": "application/json"}
     putdata = {
         "ucode": "a"+roomNumber,
         "instance": "on" if isOn else "off"
     }
-    req = requests.put(url, data=json.dumps(putdata), headers=headers, auth=(‘koshizukaLab’, ‘8TxgS73KmG’))
+    req = requests.put(url, data=json.dumps(putdata), headers=headers, auth=("koshizukaLab", "8TxgS73KmG"))
     print(req.status_code)
     
 
