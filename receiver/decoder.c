38d88fd9b00f97a6d7ff7e30606bdb0389ac4213
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-169.local>
2019-05-09 22:18:44 +0900

    aa

--- receiver/decoder.c
+++ receiver/decoder.c
@@ -16,14 +16,24 @@ int bin2dec(int *data, int length) {
 
 void decode(int* array) {
     int roomNumber = 0;
+    int isValid = 1;
 
     printf("start decoding \n");
     for(int i = 0; i < CODE_LENGTH; i++) {
         printf("%d ",array[i]);
+        if(array[i] == 3) {
+            isValid = 0;
+        }
+    }
+    printf("\n");
+
+    if(isValid == 0) {
+        printf("did not receive correctly\n");
+        return;
     }
 
     roomNumber = bin2dec(array + 2, 9);
-    printf("\ndecode result:  %d %d %d\n", array[0],array[1],roomNumber);
+    printf("decode result:  %d %d %d\n", array[0],array[1],roomNumber);
     printf("end decoding \n");
 
     if(array[0] == 0) {
