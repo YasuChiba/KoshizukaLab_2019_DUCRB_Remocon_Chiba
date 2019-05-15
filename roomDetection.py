80a826ee5bc62006a7033dc3dd98de1d92dbc732
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-264.local>
2019-05-15 20:38:13 +0900

    aa

--- roomDetection.py
+++ roomDetection.py
@@ -13,28 +13,61 @@ class WifiRouter:
         self.ssid = ssid
 
 
-while(True):
+def getRouterList():
     res=spc.check_output("getsi")
     lis=res.splitlines()
-    targetSSIDIndexList = [i for i,s in enumerate(lis) if "DUCRB-LAB" in s]
-
-
+    targetSSIDIndexList = [i for i,s in enumerate(lis) if "ESSID" in s]
     routerList = [WifiRouter(lis[i-2].split()[4],lis[i-1].strip(),lis[i].strip()) for i in targetSSIDIndexList]
+    return routerList
 
+
+routerDict = {}
+counter = 0
+
+while(True):
+    routerList = getRouterList()
     for tmp in routerList:
-        #print tmp.ssid, tmp.quality,"    ",  tmp.address
+        if tmp.address in routerDict:
+            routerDict[tmp.address][0].append(tmp)
+        else:
+            routerDict[tmp.address] = [[tmp],[]]
+
         quality = tmp.quality.split("=")[1].split("/")[0]
         signalLebel = tmp.quality.split("  ")[1].split("=")[1].split(" ")[0]
-
+        '''
         if "C0:25:5C:96:AD:D2" in tmp.address:
             print "305:", quality," ", signalLebel
         elif "64:E9:50:71:B6:52" in tmp.address:
             print "306:", quality," ", signalLebel
         elif "34:DB:FD:E1:D1:B2 " in tmp.address:
             print "307:", quality," ", signalLebel
-    print("")
-    time.sleep(5)
+            '''
+    #print("")
+
+    counter = counter + 1
+    if counter == 2:
+        break
+
+t = raw_input()
 
 
-    
+counter = 0
+while(True):
+    routerList = getRouterList()
+    for tmp in routerList:
+        if tmp.address in routerDict1:
+            routerDict1[tmp.address][1].append(tmp)
+        else:
+            routerDict1[tmp.address] = [[],[tmp]]
+
+    counter = counter + 1
+    if counter == 2:
+        break
+
+
+for address in routerDict:
+    if len(routerDict[address]) == 2:
+        mean1 = sum(routerDict[address][0])/len(routerDict[address][0])
+        mean2 = sum(routerDict[address][0])/len(routerDict[address][0])
+        print(mean1, "  ", mean2,"  ", address, "  ", routerDict[address][0][0].ssid)
 
