3bcea30c6efb5f035611981264128d3092e34319
千葉彌平 <yasuhirachiba@YasuhiraChiba-MBP-lab-341.local>
2019-05-16 17:35:40 +0900

    aa

--- receiver/segmentLED.py
+++ receiver/segmentLED.py
@@ -5,12 +5,12 @@ import sys
 
 
 args = sys.argv
-print(args)
 #args[1] : roomNumber
 
 ser = serial.Serial('/dev/serial0', 9600)
 
 roomNumber = int(args[1])
+print(roomNumber)
 ser.write(roomNumber)
 ser.close()
 
